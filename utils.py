9e80d14afccb686bb032ff6049c14d11a757e127
Florent Barre <florent.barre@kiplin.com>
2021-12-19 11:41:13 +0100

    comment utils functions

--- utils.py
+++ utils.py
@@ -11,9 +11,14 @@ class Tools:
     @staticmethod
     @st.cache
     def prepare_data():
+        """
+        Import player_info, concat first and last name, keep only player id and name.
+        Import game_skater_stats, create game year, group by player and game year, timeOnIce from second to 60 minutes
+        Merge player_info to game_skater_stats, create an uid from player id, player name and game year
+        :return: pd.DataFrame
+        """
         df_player_info = pd.read_csv('data/player_info.csv')
         df_player_info['player_name'] = df_player_info['firstName'] + '_' + df_player_info['lastName']
-        df_player_info['birth_year'] = df_player_info['birthDate'].str[:4].astype(int)
         df_player_info = df_player_info[['player_id', 'player_name']]
 
         df_game_skater_stats = pd.read_csv('data/game_skater_stats.csv')
@@ -30,6 +35,13 @@ class Tools:
 
     @staticmethod
     def acp(df, X, y):
+        """
+        Generic functions to make a PCA
+        :param df: pd.DataFrame. Dataset for PCA
+        :param X: list. Columns names for quantitative variables
+        :param y: string. Column name of variable of individuals
+        :return: df_acp, n, p, acp_, coord, eigval
+        """
         X.append(y)
         df_acp = df[X].groupby(y).mean()
 
@@ -47,6 +59,13 @@ class Tools:
 
     @staticmethod
     def get_indices_of_nearest_neighbours(df, Coords, n):
+        """
+        Calculate n nearest neighbours for each individuals with a K-D Tree
+        :param df: pd.DataFrame. df_acp
+        :param Coords: np.array. Coords od PCA
+        :param n: int. Number of nearest neighbours to calculate
+        :return: pd.DataFrame
+        """
         indice = np.array(df.index)
         tree = spatial.cKDTree(Coords)
         res = tree.query(Coords, k=n)[1][:, 0:]
@@ -55,6 +74,12 @@ class Tools:
 
     @staticmethod
     def same_reco(df, ind):
+        """
+        Display the name of nearest neighbours of a selected individuals
+        :param df: pd.DataFrame. Dataset from K-D Tree
+        :param ind: string. Selected individual
+        :return: list
+        """
         res = []
         for i in df.columns:
             res.append(df[df[0] == ind].iloc[:, i].to_string()[5:].lstrip())
@@ -65,6 +90,14 @@ class Tools:
 
     @staticmethod
     def make_df_reco_final(df_select, sel_player, df_reco, X_sel):
+        """
+
+        :param df_select: pd.DataFrame. Initial dataset from prepare_data()
+        :param sel_player: string. Selected player
+        :param df_reco: pd.DataFrame. Dataset from same_reco()
+        :param X_sel: list. Quantitatives columns to display
+        :return: pd.DataFrame
+        """
         data_reco_sel = df_select[df_select['uid'] == sel_player]
         data_reco = df_select[df_select['uid'].isin(df_reco)].drop_duplicates(subset=['uid'])
         data_reco_sort = data_reco.set_index('uid').loc[df_reco].reset_index()
